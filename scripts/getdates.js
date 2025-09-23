f6943879558a2e9bbea8d1bcfe025e837a5714e3
David Antonio Fajardo Ponce <davidagape@hotmail.com>
2025-09-23 04:01:49 -0600

    Add getDates function to display current year

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,2 +1,7 @@
-const date = new Date(document.lastModified);
-document.getElementById("lastModified").innerHTML = date;
+function getDates() {
+  var date = new Date();
+  let year = date.getFullYear();
+  document.getElementById("currentyear").innerHTML = year;
+  document.getElementById("lastModified").innerHTML = date;
+}
+getDates();
