a481c874b1c1a8b518a414301bb6700eb7f11912
David Antonio Fajardo Ponce <davidagape@hotmail.com>
2025-10-16 02:12:59 -0600

    Implement review count tracking with localStorage
    Added functionality to track review count using localStorage.

--- project/scripts/project.js
+++ project/scripts/project.js
@@ -5,3 +5,13 @@ hamburgerButton.addEventListener("click", () => {
   navMenu.classList.toggle("open");
   hamburgerButton.classList.toggle("open");
 });
+/*Use localStorage to keep track of the number of reviews completed by the user client by adding a counter 
+to the form submission review.html page. Every time that page is loaded successfully upon form submission, 
+add one to the counter.*/
+let reviewCount = localStorage.getItem("reviewCount") || 0;
+reviewCount++;
+localStorage.setItem("reviewCount", reviewCount);
+/*Uncaught TypeError: Cannot set properties of null (setting 'textContent')*/
+document.getElementById("review-count").textContent = reviewCount;
+//end of review count function
+
